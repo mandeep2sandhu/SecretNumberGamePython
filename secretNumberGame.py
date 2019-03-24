9c09b6164ed022c3d9fc23de5d117a60546c7ac0
Mandeep Sandhu <mandeep2sandhu@gmail.com>
2019-03-24 14:02:36 -0700

    Add files via upload

---
+++ secretNumberGame.py
@@ -0,0 +1,21 @@
+﻿secretNumber = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+# Write code that prompts the user to enter an integer number
+userInput = int(input("Enter a numer to guess: "))
+# Write your while loop here and the rest of your code
+while(secretNumber!=userInput):
+     print("No, that's not the number I've picked for you. Try again!")
+     userInput = int(input("Enter a different number to guess: "))
+     if (secretNumber==userInput):
+         print("Well done! That's the number I've chosen for you! You are free now.")
